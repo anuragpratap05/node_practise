4aae2e9d9e741286c05b6394255f488804650bc8
Anurag singh <anurag.singh@american-technology.net>
2024-02-14 19:23:22 +0530

    refactor

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const { myLogger } = require("./middlewares/logger");
-
-const router = express.Router();
+const { generateRandom } = require("./service/practise");
 
 const app = express();
-// app.use(express.json);
+const router = express.Router();
 app.use(express.json());
+app.use("/", router);
 app.use(myLogger);
 
 const port = process.env.APP_PORT || 6000;
 
-router.get("/", (req, res) => {
-	console.log("hitted");
-	res.send("good");
+router.get("/random", (req, res) => {
+	const {
+		body: { size },
+	} = req;
+	console.log("from get method");
+	let random = generateRandom(size);
+	res.send({ random });
 });
 
 app.listen(port, () => {
